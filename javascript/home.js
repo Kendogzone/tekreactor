2e465f16c2d9d61360ace5fd7e619c3b16387057
kendogzone <hsia.kenneth@gmail.com>
2016-03-24 17:39:46 -0700

    fucking javascript fuck

---
+++ javascript/home.js
@@ -0,0 +1,48 @@
+var main = function() {
+  $('.tagline').click(function() {
+    $('.logo'),animate(){
+    left: '0px'
+    }, 200;
+  });
+};
+
+$(document).ready(main);
+
+
+
+
+
+
+//REMEMBER this shit is all written in coffeescript so you will need to rewrite in normal js
+
+/*changePage: (event, data) -> //this will change the URL slug when they click
+  console.log event
+  Pages.transition data, true
+  history.pushState(null, document.title, $(@).attr('id')) if Pages.popping == false
+
+
+transition: (data, popping) -> //this is the animation shit
+	newWidth = $(window).width() * 0.75
+	newHeight = $(window).height() * 0.75
+	if !Pages.animating
+		$('.corner-pic').removeClass().addClass('corner-pic').attr('style', '')
+		$('#green-corner').css 'z-index', '5'
+		$('#red-corner').css 'z-index', '6'
+		$('#yellow-corner').css 'z-index', '4'
+		$('.corner-pic').addClass('animated expand')
+		$('.corner-pic').last().one 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', ->
+			Pages.animating = false
+			$('.main-content').html(data).removeClass('animated')
+			console.log data
+			console.log "data"
+			$('.main-content').children().addClass('animated fadeIn')
+			$('.corner-pic').css('height', "150%").css('width', "150%")
+			$('.corner-pic').removeClass('animated expand')
+			$('#green-corner').addClass('animated greenUnexpand')
+			$('#red-corner').addClass('animated redUnexpand')
+			$('#yellow-corner').addClass('animated yellowUnexpand')
+			$('.corner-pic').hide().hide().show()
+			$('.corner-pic').last().one 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend', ->
+				$('.corner-pic').attr('style', '')
+				$('.corner-pic').css 'z-index', '2'
+	Pages.animating = true
